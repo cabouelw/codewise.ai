9a51d7938f8ab557ea14c5e2fde811eb4a5795a2
Abouelwafa Choaib <58864054+cabouelw@users.noreply.github.com>
2025-12-11 21:47:38 +0100

    feat: Add Ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,6 +112,17 @@ export default function RootLayout({
 				<link rel="shortcut icon" href="/favicon.ico" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="manifest" href="/site.webmanifest" />
+			 
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8652734840791356" crossorigin="anonymous"></script>
+				<ins class="adsbygoogle"
+					style="display:block; text-align:center;"
+					data-ad-layout="in-article"
+					data-ad-format="fluid"
+					data-ad-client="ca-pub-8652734840791356"
+					data-ad-slot="1031248666"></ins>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
 
 				{/* Organization Structured Data */}
 				<JsonLd
